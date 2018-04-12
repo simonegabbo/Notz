df80581d1cef38aea27dec3d25a7ce04a86dccca
Amine Nokra <amine.nokra@gmail.com>
2018-04-12 19:01:51 +0200

    project structure and recycler view

--- app/src/main/java/eu/fse/notz/MainActivity.java
+++ app/src/main/java/eu/fse/notz/MainActivity.java
@@ -2,12 +2,35 @@ package eu.fse.notz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private String[] myDataset = {"nota 1"," nota 2", "fai la spesa", "paga bolletta luca", "dadsadasa", "dsasdasd", "dassad"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.notes_rv);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        // specify an adapter (see also next example)
+        mAdapter = new NotesAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+
     }
 }
--- app/src/main/java/eu/fse/notz/NotesAdapter.java
+++ app/src/main/java/eu/fse/notz/NotesAdapter.java
@@ -1,8 +1,59 @@
 package eu.fse.notz;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by amine on 12/04/18.
  */
 
-public class NotesAdapter {
+public class NotesAdapter extends RecyclerView.Adapter {
+
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public NotesAdapter(String[] myDataset){
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_note, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        NotesAdapter.ViewHolder  noteVH = (NotesAdapter.ViewHolder)holder;
+        noteVH.mTextView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
