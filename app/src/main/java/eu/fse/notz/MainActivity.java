44e1b2d37fc61ea50b544fdb00b56f6b5219010b
Amine Nokra <amine.nokra@gmail.com>
2018-04-19 19:03:18 +0200

    floating action button,dialog for add note

--- app/src/main/java/eu/fse/notz/MainActivity.java
+++ app/src/main/java/eu/fse/notz/MainActivity.java
@@ -1,17 +1,23 @@
 package eu.fse.notz;
 
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private NotesAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private FloatingActionButton addNoteButton;
 
    // private String[] myDataset = {"nota 1"," nota 2", "fai la spesa", "paga bolletta luca", "dadsadasa", "dsasdasd", "dassad"};
     private ArrayList<Note> myDataset;
@@ -25,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
+
+        addNoteButton = findViewById(R.id.add_note_fab);
 
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
@@ -42,5 +50,52 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
 
 
+        addNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               showDialog();
+
+            }
+        });
+
     }
+
+
+
+
+
+    private void showDialog(){
+
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+
+        alertBuilder.setView(R.layout.dialog_add_note);
+        alertBuilder.setTitle(R.string.dialog_add_note_title);
+
+        alertBuilder.setPositiveButton(R.string.dialog_positive_button,
+                new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //
+                Note note = new Note("Titolo della nota",
+                        "contenuto nota dsaasd");
+                mAdapter.addNote(note);
+
+            }
+        });
+
+        alertBuilder.setNegativeButton(R.string.dialog_negative_button,
+                new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //
+            }
+        });
+
+        alertBuilder.show();
+
+
+    }
+
+
 }
--- app/src/main/java/eu/fse/notz/NotesAdapter.java
+++ app/src/main/java/eu/fse/notz/NotesAdapter.java
@@ -1,7 +1,9 @@
 package eu.fse.notz;
 
+import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -20,10 +22,13 @@ public class NotesAdapter extends RecyclerView.Adapter {
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
+        public TextView titleTv;
+        public TextView descriptionTv;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            titleTv = (TextView) itemView.findViewById(R.id.title_tv);
+            descriptionTv = (TextView) itemView.findViewById(R.id.description_tv);
+
         }
     }
 
@@ -32,12 +37,20 @@ public class NotesAdapter extends RecyclerView.Adapter {
         mDataset = myDataset;
     }
 
+
+    public void addNote(Note note){
+        mDataset.add(note);
+        notifyDataSetChanged();
+       // notifyItemInserted();
+
+    }
+
     // Create new views (invoked by the layout manager)
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        View v =  LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.row_note, parent, false);
 
         ViewHolder vh = new ViewHolder(v);
@@ -52,7 +65,12 @@ public class NotesAdapter extends RecyclerView.Adapter {
         // - replace the contents of the view with that element
         NotesAdapter.ViewHolder  noteVH = (NotesAdapter.ViewHolder)holder;
         Note currentNote = mDataset.get(position);
-        noteVH.mTextView.setText(currentNote.getTitle());
+
+        // Data binding
+        noteVH.titleTv.setText(currentNote.getTitle());
+        noteVH.descriptionTv.setText(currentNote.getDescription());
+
+
     }
 
     @Override
