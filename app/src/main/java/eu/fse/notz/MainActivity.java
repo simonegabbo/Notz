e9490c938e6a4e8d6742e85931504466d9fb5405
Amine Nokra <amine.nokra@gmail.com>
2018-04-24 17:57:11 +0200

    onclick on itemview

--- app/src/main/java/eu/fse/notz/MainActivity.java
+++ app/src/main/java/eu/fse/notz/MainActivity.java
@@ -8,7 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     private FloatingActionButton addNoteButton;
 
-   // private String[] myDataset = {"nota 1"," nota 2", "fai la spesa", "paga bolletta luca", "dadsadasa", "dsasdasd", "dassad"};
+    // private String[] myDataset = {"nota 1"," nota 2", "fai la spesa", "paga bolletta luca", "dadsadasa", "dsasdasd", "dassad"};
     private ArrayList<Note> myDataset;
 
     @Override
@@ -34,19 +37,15 @@ public class MainActivity extends AppCompatActivity {
 
         addNoteButton = findViewById(R.id.add_note_fab);
 
-        mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager = new StaggeredGridLayoutManager(2,
+                StaggeredGridLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         myDataset = new ArrayList<>();
 
-        Note pinPalazzo = new Note("PIN","12345");
-        myDataset.add(pinPalazzo);
-
-        Note spesa = new Note("Spesa","comprare il latte");
-        myDataset.add(spesa);
 
         // specify an adapter (see also next example)
-        mAdapter = new NotesAdapter(myDataset);
+        mAdapter = new NotesAdapter(myDataset,this);
         mRecyclerView.setAdapter(mAdapter);
 
 
@@ -54,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-               showDialog();
+                showDialog();
 
             }
         });
@@ -62,35 +61,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private void showDialog() {
 
+        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
 
+        final View dialogView = LayoutInflater.from(this)
+                .inflate(R.layout.dialog_add_note,null);
 
-    private void showDialog(){
-
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-
-        alertBuilder.setView(R.layout.dialog_add_note);
+        alertBuilder.setView(dialogView);
         alertBuilder.setTitle(R.string.dialog_add_note_title);
+
+        final EditText titleEt = dialogView.findViewById(R.id.dialog_title_et);
+        final EditText descriptionEt = dialogView.findViewById(R.id.dialog_description_et);
 
         alertBuilder.setPositiveButton(R.string.dialog_positive_button,
                 new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //
-                Note note = new Note("Titolo della nota",
-                        "contenuto nota dsaasd");
-                mAdapter.addNote(note);
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //
 
-            }
-        });
+                        String insertedTitle = titleEt.getText().toString();
+                        String insertedDescription = descriptionEt.getText().toString();
+
+                        Note note = new Note(insertedTitle,
+                                insertedDescription);
+                        mAdapter.addNote(note);
+
+                    }
+                });
 
         alertBuilder.setNegativeButton(R.string.dialog_negative_button,
                 new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //
-            }
-        });
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //
+                    }
+                });
 
         alertBuilder.show();
 
--- app/src/main/java/eu/fse/notz/NotesAdapter.java
+++ app/src/main/java/eu/fse/notz/NotesAdapter.java
@@ -1,12 +1,16 @@
 package eu.fse.notz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.NotActiveException;
 import java.util.ArrayList;
 
 /**
@@ -16,32 +20,47 @@ import java.util.ArrayList;
 public class NotesAdapter extends RecyclerView.Adapter {
 
     private ArrayList<Note> mDataset;
+    private Context context;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public TextView titleTv;
         public TextView descriptionTv;
+
+
         public ViewHolder(View itemView) {
             super(itemView);
             titleTv = (TextView) itemView.findViewById(R.id.title_tv);
             descriptionTv = (TextView) itemView.findViewById(R.id.description_tv);
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //TODO fai cose
+                    Intent intent = new Intent(context, NoteActivity.class);
+                    context.startActivity(intent);
+
+                }
+            });
+
         }
+
+
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public NotesAdapter(ArrayList<Note> myDataset){
+    public NotesAdapter(ArrayList<Note> myDataset,Context context) {
         mDataset = myDataset;
+        this.context = context;
     }
 
 
-    public void addNote(Note note){
-        mDataset.add(note);
-        notifyDataSetChanged();
-       // notifyItemInserted();
+    public void addNote(Note note) {
+        this.mDataset.add(0, note);
+        notifyItemInserted(0);
 
     }
 
@@ -50,7 +69,7 @@ public class NotesAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        View v =  LayoutInflater.from(parent.getContext())
+        View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.row_note, parent, false);
 
         ViewHolder vh = new ViewHolder(v);
@@ -63,7 +82,7 @@ public class NotesAdapter extends RecyclerView.Adapter {
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        NotesAdapter.ViewHolder  noteVH = (NotesAdapter.ViewHolder)holder;
+        NotesAdapter.ViewHolder noteVH = (NotesAdapter.ViewHolder) holder;
         Note currentNote = mDataset.get(position);
 
         // Data binding
